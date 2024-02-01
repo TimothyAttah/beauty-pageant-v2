da24188c759c2439d6bac197bedeb3c66cf310af
Timothy Attah <timothyattah733@gmail.com>
2024-02-01 11:30:19 +0100

    Added fadein effects again.

--- frontend/src/pages/contestant/Contestants.jsx
+++ frontend/src/pages/contestant/Contestants.jsx
@@ -4,6 +4,7 @@ import { FaUserCircle } from 'react-icons/fa';
 import * as Styles from './ContestantStyles';
 import { getAllContestant } from '../../redux/actions/contestantActions';
 import { Link } from 'react-router-dom';
+import { FadeIn } from '../../components/fadeIn/FadeIn';
 
 export const Contestants = () => {
   const dispatch = useDispatch();
@@ -18,21 +19,23 @@ export const Contestants = () => {
   return (
     <Styles.ContestantContainer>
       <Styles.ContestantWrapper>
-        {constestants?.map((contestant) => (
+        {constestants?.map((contestant, i) => (
           <Link key={contestant?._id} to={`/profile/${contestant?._id}`}>
-            <Styles.ProfileCard>
-              <div>{contestant?.profilePicture ? '' : <FaUserCircle />}</div>
-              <h1>{contestant?.contestantName}</h1>
-              <p>
-                Email: <span>{contestant?.email}</span>
-              </p>
-              <p>
-                Phone Number: <span>{contestant?.phoneNumber}</span>
-              </p>
-              <p>
-                Age: <span>{contestant?.age}</span>
-              </p>
-            </Styles.ProfileCard>
+            <FadeIn delay={(i + 1) * 0.2} direction='right'>
+              <Styles.ProfileCard>
+                <div>{contestant?.profilePicture ? '' : <FaUserCircle />}</div>
+                <h1>{contestant?.contestantName}</h1>
+                <p>
+                  Email: <span>{contestant?.email}</span>
+                </p>
+                <p>
+                  Phone Number: <span>{contestant?.phoneNumber}</span>
+                </p>
+                <p>
+                  Age: <span>{contestant?.age}</span>
+                </p>
+              </Styles.ProfileCard>
+            </FadeIn>
           </Link>
         ))}
       </Styles.ContestantWrapper>
